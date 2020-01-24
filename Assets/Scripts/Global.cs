68d88d346d7ba4763a46a1b27dfb47906c165119
Tea Tran <tatran@seas.upenn.edu>
2020-01-24 10:09:40 -0500

    nearly finished invader -- need to add die condition and constructors

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    // The invaders can only move horizontally within this boundary
+    public float lastTimeStep;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTimeStep = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastTimeStep = Time.time;
+    }
+
+    public void Lose()
+    {
+
+    }
+}
